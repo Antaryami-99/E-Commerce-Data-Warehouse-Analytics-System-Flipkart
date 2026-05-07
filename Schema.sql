e833503684b494d0ed8b13a6db977354864794b5
Antaryami Moharana <antaryamimoharana@192.168.1.7>
2026-05-08 01:13:50 +0530

    your commit message here

---
+++ Schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE customers_ecommerce (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    created_at DATE
+);
+
---
+++ sample_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customers_ecommerce VALUES
+(1,'Rahul Sharma','rahul@email.com','9999999999','Bangalore','Karnataka','2021-01-01'),
+(2,'Priya Singh','priya@email.com','8888888888','Mumbai','Maharashtra','2021-03-11');
