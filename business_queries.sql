d8b7da36397171dcf181cc9fff91edfe3789a279
Antaryami Moharana <antaryamimoharana@192.168.1.7>
2026-05-25 18:59:54 +0530

    Force update schema file

--- business_queries.sql
+++ business_queries.sql
@@ -12,3 +12,43 @@ product_name,total_sold
 'Samsung S21','2'
 'iPhone 13','1'
 
+2️⃣ Revenue by Month
+
+SELECT DATE_FORMAT(order_date, '%Y-%m-%d') AS month,
+SUM(total_amount) AS monthly_revenue
+FROM orders_ecommerce
+GROUP BY month
+ORDER BY month;
+
+Output:
+month,monthly_revenue
+'2023-01-01','124999.00'
+'2023-02-01','54999.00'
+
+3️⃣ Customer Lifetime Value (LTV)
+
+select c.customer_id,c.customer_name,
+sum(o.total_amount) as lifetime_value
+from customers_ecommerce c 
+join orders_ecommerce o on c.customer_id =o.customer_id
+group by c.customer_id,c.customer_name
+order by lifetime_value desc;
+
+Output:
+customer_id,customer_name,lifetime_value
+'1','Rahul Sharma','124999.00'
+'2','Priya Singh','54999.00'
+
+
+4️⃣ Window Function – Ranking Top Customers
+
+select customer_id,sum(total_amount),
+rank() over(order by sum(total_amount) desc) as rn
+from orders_ecommerce
+group by customer_id;
+
+Output:
+customer_id,sum(total_amount),rn
+'1','124999.00','1'
+'2','54999.00','2'  
+
